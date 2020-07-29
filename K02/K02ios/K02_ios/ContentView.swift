0253535512d65c8b10ad253cec40129c4a62d88b
martin <martin@randelzofer.de>
2020-07-29 15:30:02 +0200

    Added K02ios

---
+++ K02/K02ios/K02_ios/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("x").font(.system(size:100))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
